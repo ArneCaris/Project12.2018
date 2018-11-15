261b92009cfdebd128a5aedab152bde4618386e0
OleksandrGudenko <oleksandr.gudenko@gmail.com>
2018-11-15 09:33:32 +0200

    API finilized.

--- express_api/models/Shared.js
+++ express_api/models/Shared.js
@@ -1,18 +1,22 @@
 var db = require('../database');
-var Shareds = {
+var Shared = {
   getSharedPostByViewer: function(Viewer, callback) {
-    return db.query('select * from sharedpost where Viewer=?',[Viewer], callback);
+    return db.query('select * from sharedpost where Viewer=?',
+    [Viewer], callback);
   },
   getSharedPostByOwner: function(Owner, callback) {
-    return db.query('select * from sharedpost where Owner=?',[Owner], callback);
+    return db.query('select * from sharedpost where Owner=?',
+    [Owner], callback);
   },
   addSharedPost: function(sharedpost, callback) {
     return db.query(
         'insert into sharedpost values (?,?,?,?)',
-        [sharedpost.ID, sharedpost.PostID, sharedpost.Owner, sharedpost.Viewer], callback);
+        [sharedpost.ID, sharedpost.PostID, sharedpost.Owner,
+         sharedpost.Viewer], callback);
   },
   deleteSharedPost(ID, callback){
-    return db.query('delete from sharedpost where ID=?', [ID], callback);
+    return db.query('delete from sharedpost where ID=?',
+    [ID], callback);
   },
 };
-module.exports = Shareds;
+module.exports = Shared;
