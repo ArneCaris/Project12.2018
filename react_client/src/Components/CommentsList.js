94857317f872449a54ea423512cb235178ea887f
unknown <Japatius_96@hotmail.com>
2018-12-02 16:42:57 +0200

    Developing posts

---
+++ react_client/src/Components/CommentsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentsList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CommentsList;
--- react_client/src/test.js
+++ react_client/src/test.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import OptionModal from './Components/OptionModal';
-import { Badge } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Badge } from 'reactstrap';
 import { Route, Link } from 'react-router-dom';
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Wholepost from './Components/Wholepost';
 import NavigationButton from './Components/NavigationButton';
-
+import CommentField from './Components/CommentField';
+import PostModal from './Components/PostModal';
 import RemoveButton from './Components/RemoveButton';
 
 class Test extends Component {
@@ -20,14 +20,29 @@ class Test extends Component {
     this.state = {
       posts: [],
       ID: '',
+      Title: '',
+      Content: '',
+      LastEdit: '',
+      modal: false
     };
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleModal = this.handleModal.bind(this);
   }
 
   componentDidMount() {
     axios.get(`http://localhost:3000/posts/public`).then(results => {
       const posts = results.data;
-      this.setState({ posts });
+      const ID = results.data;
+      const Title = results.data;
+      const Content = results.data;
+      const LastEdit = results.data;
+      this.setState({ 
+        posts,
+        ID,
+        Title,
+        Content,
+        LastEdit,
+       });
     });
   }
 
@@ -37,26 +52,40 @@ class Test extends Component {
     });
   };
 
+  handleModal() {
+    this.setState({
+        modal: !this.state.modal,
+        ID: this.state.ID,
+        Title: this.state.Title,
+
+    });
+}
+
 
 render() {
-
+    let postsList = this.state.posts.map ((Post, i) => {
+        return (
+        <div className="for-posts" data-id={Post.ID}>
+        <div className="postdiv">
+          <ul>
+            <li onClick={this.handleModal} key={i}>{Post.Title}</li>
+            <li key={i}>{Post.Content}</li>
+          </ul>
+        </div>
+        </div>)
+      });
     return(
       <div>
-        <div className="for-posts">
-            {this.state.posts.map(Post =>(
-            <ul>
-              <div className="postdiv" data-id={Post.ID}>
-              <RemoveButton {...Post} removePost={this.handleRemove} />
-              <p className="for-id">#{Post.ID} Last edited: {Post.LastEdit}</p>
-                <h4>{Post.Title}</h4><Badge color="info" className="category-badge">{Post.Category}</Badge>
-              <p className="text">{Post.Content}</p>
-              <section>
-                <NavigationButton/>
-              </section>
-              </div>
-            </ul>
-            ))}
+        <div>
+          {postsList}
         </div>
+        <Modal key={this.props.ID} isOpen={this.state.modal} toggle={this.handleModal} key={this.state.posts.ID} className="modalpost">
+            <ModalHeader>{this.props.Title}</ModalHeader>
+            <ModalBody>{this.props.Content}</ModalBody>
+            
+              <CommentField/>
+            
+          </Modal>
       </div>
     );
 }
